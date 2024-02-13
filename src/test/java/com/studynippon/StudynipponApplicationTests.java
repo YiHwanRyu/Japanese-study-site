00310606db0a22f7a56f6f740a36d9c308a55911
YiHwanRyu <yihwandev@gmail.com>
2024-02-13 23:23:20 +0900

    [ADD] Project 초기 설정

---
+++ src/test/java/com/studynippon/StudynipponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studynippon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudynipponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
